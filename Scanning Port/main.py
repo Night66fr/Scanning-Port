a19c20ef6f95e131a4e4e725c3a7aced1cf1380e
Stan <158843492+Night66fr@users.noreply.github.com>
2026-02-24 00:41:12 +0100

    V2 Working (1 to 1024 port)

--- Scanning Port/main.py
+++ Scanning Port/main.py
@@ -2,7 +2,7 @@
 import socket
 
 def ip_ask():
-    #input ip
+    # input ip
     print('Ip format : x.x.x.x')
     print('Each x is between 1 to 3')
     ip_chosen = input('What ip do you want to scan ? (ipv4)\n')
@@ -11,7 +11,11 @@ def ip_ask():
 def port_try():
     # tcp connection  = open
     # no tcp connection = close
-    port_chosen = int(input('Do you want to enter a specific port ? (enter to skip)\n'))
+    port_chosen = input('Do you want to enter a specific port ? (enter to skip and scan all)\n')
+    if port_chosen == '':
+        port_chosen = port_chosen
+    else:
+        port_chosen = int(port_chosen)
     return port_chosen
     
 
@@ -20,20 +24,42 @@ def open_close(ip_chosen, port_chosen):
     test_port.settimeout(1)
     test_port_result = test_port.connect_ex((ip_chosen, port_chosen))
     if test_port_result == 0:
-        print(f'Le port {port_chosen} est ouvert')
+        print(f'Port {port_chosen} is open')
+        test_port.close()
+        return 0
     else:
-        print(f'Le port {port_chosen} est fermée')
+        # print(f'Port {port_chosen} is closed') # Commented out to avoid spamming the console
+        test_port.close() 
+        return 1
 
 
 # loop 1 to 1024
 def main():
     target_address = ip_ask()
     target_port = port_try()
+    ouvert = 0
+    ferme = 0
+
     print(f'You chose to scan the ip address : {target_address}')
+
     if target_port == '':
         print('You chose not to put any port')
+        print('The scan of all ports has begun')
+        for port in range(1, 1025):
+            resultat = open_close(target_address, port)
+            if resultat == 0:
+                ouvert += 1
+            else:
+                ferme += 1
+
     else:
         print(f'You chose to scan the port : {target_port}')
-    open_close(target_address, target_port)
+        resultat = open_close(target_address, target_port)
+        if resultat == 0:
+            ouvert += 1
+        else:
+            ferme += 1
+
+    print(f"There are {ouvert} open ports and {ferme} closed ports")
 
 main()
