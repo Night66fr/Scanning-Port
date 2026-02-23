0a93f0fc36c6316fedcb8b8aa7e38c571a1818fe
Stan <158843492+Night66fr@users.noreply.github.com>
2026-02-23 23:58:01 +0100

    socket start

--- Scanning Port/main.py
+++ Scanning Port/main.py
@@ -1,28 +1,32 @@
 # Scanning for port 
+import socket
 
 def ip_ask():
     #input ip
     print('Ip format : x.x.x.x')
     print('Each x is between 1 to 3')
     ip_chosen = input('What ip do you want to scan ? (ipv4)\n')
-    print(f'You chose to scan the ip address : {ip_chosen}')
     return ip_chosen
 
 def port_try():
     # tcp connection  = open
     # no tcp connection = close
-    x = input('Do you want to enter a specific port ? (enter to skip)\n')
-    if x == '':
-        print('You chose not to put any port')
-    else:
-        print(f'You chose to scan the port : {x}')
+    port_chosen = input('Do you want to enter a specific port ? (enter to skip)\n')
+    return port_chosen
+    
 
 def open_close():
     pass
 
 # loop 1 to 1024
+def main():
+    target_address = ip_ask()
+    target_port = port_try()
+    print(f'You chose to scan the ip address : {target_address}')
+    if target_port == '':
+        print('You chose not to put any port')
+    else:
+        print(f'You chose to scan the port : {target_port}')
+    open_close()
 
-address = ip_ask()
-print(f'The ip chosen in the fonction is {address}')
-port_try()
-open_close()
+main()
